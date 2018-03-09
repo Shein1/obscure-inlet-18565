4729bf967db4c6a9f7d1eb61aa4b3ef6a1a90749
Shein1 <sheikh.rohman.20170799@efreitech.net>
2018-03-09 12:19:51 +0100

    Add !

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({hi: 'Hello World'});
+  res.json({hi: 'Hello World !'});
 });
 
 module.exports = router;
